765dab1541a72fd7694d4ffd7b552aa212d975d2
theo10111986 <th.kodizas@gmail.com>
2025-08-27 17:14:21 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -6,11 +6,18 @@ const nextConfig: NextConfig = {
     // Don't fail the production build because of ESLint errors
     ignoreDuringBuilds: true,
   },
-  // NOTE: If you ever get stuck on *TypeScript* build errors (not recommended),
-  // you can temporarily uncomment this:
-  // typescript: {
-  //   ignoreBuildErrors: true,
-  // },
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        // αν ο host είναι kzsyndicate.com (χωρίς www)
+        has: [{ type: "host", value: "kzsyndicate.com" }],
+        // στείλ' τον στο www με ίδιο path
+        destination: "https://www.kzsyndicate.com/:path*",
+        permanent: true, // 301
+      },
+    ];
+  },
 };
 
 export default nextConfig;
