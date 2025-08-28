63d8c286951d46bbecf44ff70a2c22035a8e3ae8
theo10111986 <th.kodizas@gmail.com>
2025-08-28 13:46:15 +0300

    Update page.tsx

--- app/shop/custom/page.tsx
+++ app/shop/custom/page.tsx
@@ -1,11 +1,47 @@
-// app/shop/custom/page.tsx
+import Script from "next/script";
+
 export const metadata = {
   title: "Custom Yourself | KZ Syndicate",
   description: "Διάλεξε υποκατηγορία (Angelus, Πρόσθετα/Προεργασία) και ξεκίνα το customization σου.",
 };
 
-import CustomClient from "./CustomClient";
-
 export default function CustomLandingPage() {
-  return <CustomClient />;
+  return (
+    <>
+      {/* ...το UI σου (ή <CustomClient />) ... */}
+
+      <Script
+        id="breadcrumbs-shop-custom"
+        type="application/ld+json"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify({
+            "@context": "https://schema.org",
+            "@type": "BreadcrumbList",
+            "itemListElement": [
+              {
+                "@type": "ListItem",
+                "position": 1,
+                "name": "Home",
+                "item": "https://www.kzsyndicate.com/"
+              },
+              {
+                "@type": "ListItem",
+                "position": 2,
+                "name": "Shop",
+                "item": "https://www.kzsyndicate.com/shop"
+              },
+              {
+                "@type": "ListItem",
+                "position": 3,
+                "name": "Custom",
+                "item": "https://www.kzsyndicate.com/shop/custom"
+              }
+            ]
+          }),
+        }}
+      />
+    </>
+  );
 }
+
