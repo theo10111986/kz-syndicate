6d357df7db7515f8dc821b70d3d1bad1640e58fc
theo10111986 <th.kodizas@gmail.com>
2025-08-28 13:47:14 +0300

    Update page.tsx

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -1,12 +1,40 @@
-// app/shop/page.tsx
+import Script from "next/script";
+
 export const metadata = {
   title: "Shop | KZ Syndicate",
   description: "Ανακάλυψε custom sneakers, clothes, accessories & custom tools.",
 };
 
-import ShopClient from "./ShopClient";
-
 export default function ShopPage() {
-  return <ShopClient />;
-}
+  return (
+    <>
+      {/* ...το UI σου (ή <ShopClient />) ... */}
 
+      <Script
+        id="breadcrumbs-shop"
+        type="application/ld+json"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify({
+            "@context": "https://schema.org",
+            "@type": "BreadcrumbList",
+            "itemListElement": [
+              {
+                "@type": "ListItem",
+                "position": 1,
+                "name": "Home",
+                "item": "https://www.kzsyndicate.com/"
+              },
+              {
+                "@type": "ListItem",
+                "position": 2,
+                "name": "Shop",
+                "item": "https://www.kzsyndicate.com/shop"
+              }
+            ]
+          }),
+        }}
+      />
+    </>
+  );
+}
