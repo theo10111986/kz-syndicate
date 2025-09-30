a6fec4c63a69b900c33fdba5ba01191e1dd1fcee
theo10111986 <th.kodizas@gmail.com>
2025-09-30 13:33:51 +0300

    Update page.tsx

--- app/checkout/test/page.tsx
+++ app/checkout/test/page.tsx
@@ -22,33 +22,19 @@ export default function TestCheckoutPage() {
         action="https://paycenter.piraeusbank.gr/redirection/pay.aspx"
       >
         <input type="hidden" name="AcquirerId" value="14" />
-        <input type="hidden" name="MerchantId" value="2145101053" />
-        <input type="hidden" name="PosId" value="2143820869" />
-        <input type="hidden" name="User" value="TH695378" />
-        <input type="hidden" name="Password" value="4db1f54e6f0b56080a51d99ec55d9bda" />
-        <input
-          type="hidden"
-          name="MerchantReference"
-          value={"TEST-" + Date.now()}
-        />
-        <input type="hidden" name="RequestType" value="02" />
-        <input type="hidden" name="CurrencyCode" value="978" />
-        <input type="hidden" name="Amount" value="100" /> {/* 1.00 EUR */}
-        <input type="hidden" name="Installments" value="0" />
-        <input
-          type="hidden"
-          name="ParamBackLink"
-          value="https://www.kzsyndicate.com/cart"
-        />
-        <input
-          type="hidden"
-          name="ParamSuccessURL"
-          value="https://www.kzsyndicate.com/api/checkout/payment/success"
-        />
-        <input
-          type="hidden"
-          name="ParamFailureURL"
-          value="https://www.kzsyndicate.com/api/checkout/payment/failure"
+<input type="hidden" name="MerchantId" value="2145101053" />
+<input type="hidden" name="PosId" value="2143820869" />
+<input type="hidden" name="User" value="TH695378" />
+<input type="hidden" name="Password" value="4db1f54e6f0b56080a51d99ec55d9bda" />
+<input type="hidden" name="MerchantReference" value="TEST-<μοναδικό νούμερο>" />
+<input type="hidden" name="RequestType" value="02" />
+<input type="hidden" name="CurrencyCode" value="978" />
+<input type="hidden" name="Amount" value="100" />
+<input type="hidden" name="Installments" value="0" />
+<input type="hidden" name="ParamBackLink" value="https://www.kzsyndicate.com/cart" />
+<input type="hidden" name="ParamSuccessURL" value="https://www.kzsyndicate.com/api/checkout/payment/success" />
+<input type="hidden" name="ParamFailureURL" value="https://www.kzsyndicate.com/api/checkout/payment/failure" />
+
         />
 
         <noscript>
