de071f9acbe559008b5e3427474afdab809760ec
theo10111986 <th.kodizas@gmail.com>
2025-09-30 11:44:17 +0300

    Create page.tsx

---
+++ app/checkout/payment/success/page.tsx
@@ -0,0 +1,8 @@
+export default function SuccessPage() {
+  return (
+    <main style={{ textAlign: "center", paddingTop: 100, color: "lime" }}>
+      <h1>Η πληρωμή ολοκληρώθηκε με επιτυχία ✅</h1>
+      <p>Ευχαριστούμε!</p>
+    </main>
+  );
+}
