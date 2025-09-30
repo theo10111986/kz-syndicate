40224784ad818eae6c4bd647d66802abf68ede5f
theo10111986 <th.kodizas@gmail.com>
2025-09-30 11:45:05 +0300

    Create page.tsx

---
+++ app/checkout/payment/cancel/page.tsx
@@ -0,0 +1,7 @@
+export default function CancelPage() {
+  return (
+    <main style={{ textAlign: "center", paddingTop: 100, color: "orange" }}>
+      <h1>Η πληρωμή ακυρώθηκε από τον χρήστη 🚫</h1>
+    </main>
+  );
+}
