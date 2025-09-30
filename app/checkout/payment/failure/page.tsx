2034dab313f248c3a3e5aee19a166d538b64e8ed
theo10111986 <th.kodizas@gmail.com>
2025-09-30 11:44:43 +0300

    Create page.tsx

---
+++ app/checkout/payment/failure/page.tsx
@@ -0,0 +1,8 @@
+export default function FailurePage() {
+  return (
+    <main style={{ textAlign: "center", paddingTop: 100, color: "red" }}>
+      <h1>Η πληρωμή απέτυχε ❌</h1>
+      <p>Δοκίμασε ξανά ή άλλαξε κάρτα.</p>
+    </main>
+  );
+}
